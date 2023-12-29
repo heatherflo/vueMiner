719ce6a110ede1ab0b73981dc08e88f40f185a88
Heather Floyd <heatherflo@gmail.com>
2023-12-29 08:12:10 -0700

    appstate complete

--- src/AppState.js
+++ src/AppState.js
@@ -1,4 +1,5 @@
 import { reactive } from 'vue'
+import { Upgrade } from './models/Upgrade.js'
 
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
@@ -10,14 +11,49 @@ export const AppState = reactive({
 
   clickUpgrades: [
     new Upgrade({
-      name: 'Pong Blade',
+      name: 'kitty-friend',
       qty: 0,
       multiplier: 1,
-      price: 50,
-      picture: '🏓'
+      price: 20,
+      picture: '🐱'
     }),
-  ]
 
+    new Upgrade({
+      name: 'mouse-friend',
+      qty: 0,
+      multiplier: 3,
+      price: 50,
+      picture: '🐭'
+    }),
+
+    new Upgrade({
+      name: 'monkey-friend',
+      qty: 0,
+      multiplier: 5,
+      price: 100,
+      picture: '🙊'
+
+    }),
+
+  ],
+
+  autoUpgrades: [
+    new Upgrade({
+      name: 'elephant-friend',
+      qty: 0,
+      multiplier: 10,
+      price: 500,
+      picture: '🐘'
+    }),
+
+    new Upgrade({
+      name: 'giraffe-friend',
+      qty: 0,
+      multiplier: 20,
+      price: 1000,
+      picture: '🦒'
+    })
+  ]
 
 
 })
